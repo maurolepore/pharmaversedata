bec6f7e7c80b2f6439cea7c839d3d747ae5d5381
Mauro Lepore <maurolepore@gmail.com>
2025-03-17 18:02:46 +0000

    wip

--- R/pharmaversedata-package.R
+++ R/pharmaversedata-package.R
@@ -8,6 +8,10 @@
 NULL
 
 ## usethis namespace: start
+#' @importFrom teal init
+#' @importFrom teal modify_footer
+#' @importFrom teal modify_header
+#' @importFrom teal modify_title
 #' @importFrom teal.data teal_data
 #' @importFrom teal.modules.general tm_data_table
 ## usethis namespace: end
