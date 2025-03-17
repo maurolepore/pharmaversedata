98bb13753d39739114782a217d47d77e6281e97d
Mauro Lepore <maurolepore@gmail.com>
2025-03-17 16:40:24 +0100

    New adam and sdtm specs (#1)
    * New adam and sdtm specs
    
    * Style

--- data-raw/data.R
+++ data-raw/data.R
@@ -1,17 +1,35 @@
+library(dplyr)
+library(tibble)
+library(readxl)
+library(dverse)
+library(janitor)
+library(usethis)
 library(pharmaversesdtm)
 library(pharmaverseadam)
+
+standardize <- function(data) {
+  data |>
+    janitor::clean_names() |>
+    relocate(
+      matches("dataset|name"),
+      matches("label"),
+      matches("variable")
+    ) |>
+    tibble::as_tibble()
+}
 
 universe <- c("pharmaversesdtm", "pharmaverseadam")
 template <- "https://pharmaverse.github.io/{package}/reference/{topic}.html"
 pharmadverse <- dverse::document_universe(universe, template)
-
 usethis::use_data(pharmadverse, overwrite = TRUE)
 
-path <- system.file("extdata/adams-specs.xlsx", package = "pharmaverseadam")
-specs <- list(
-  define = readxl::read_xlsx(path, "Define") |> janitor::clean_names(),
-  datasets = readxl::read_xlsx(path, "Datasets") |> janitor::clean_names(),
-  variables = readxl::read_xlsx(path, "Variables") |> janitor::clean_names()
-)
+xlsx <- system.file("extdata", "adams-specs.xlsx", package = "pharmaverseadam")
+adam_datasets <- readxl::read_xlsx(xlsx, "Datasets") |> standardize()
+usethis::use_data(adam_datasets, overwrite = TRUE)
 
-usethis::use_data(specs, overwrite = TRUE)
+adam_variables <- readxl::read_xlsx(xlsx, "Variables") |> standardize()
+usethis::use_data(adam_variables, overwrite = TRUE)
+
+json <- system.file("extdata", "sdtms-specs.json", package = "pharmaversesdtm")
+sdtm_datasets <- tibble::tibble(jsonlite::fromJSON(json)[[1]]) |> standardize()
+usethis::use_data(sdtm_datasets, overwrite = TRUE)
